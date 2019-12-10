9a1ae4ef5fb9e9abf37200c220a8287c52c3b418
strangenamebc <5533827+strangenamebc@user.noreply.gitee.com>
2019-12-10 10:49:37 +0800

    格式化代码

--- RM_Manager.cpp
+++ RM_Manager.cpp
@@ -1,48 +1,49 @@
 #include "stdafx.h"
 #include "RM_Manager.h"
 #include "str.h"
+#include "PF_Manager.h"
 
-
-RC OpenScan(RM_FileScan *rmFileScan,RM_FileHandle *fileHandle,int conNum,Con *conditions)//初始化扫描
+RC OpenScan(RM_FileScan* rmFileScan, RM_FileHandle* fileHandle, int conNum, Con* conditions)//初始化扫描
 {
 	return SUCCESS;
 }
 
-RC GetNextRec(RM_FileScan *rmFileScan,RM_Record *rec)
+RC GetNextRec(RM_FileScan* rmFileScan, RM_Record* rec)
 {
 	return SUCCESS;
 }
 
-RC GetRec (RM_FileHandle *fileHandle,RID *rid, RM_Record *rec) 
+RC GetRec(RM_FileHandle* fileHandle, RID* rid, RM_Record* rec)
 {
 	return SUCCESS;
 }
 
-RC InsertRec (RM_FileHandle *fileHandle,char *pData, RID *rid)
+RC InsertRec(RM_FileHandle* fileHandle, char* pData, RID* rid)
 {
 	return SUCCESS;
 }
 
-RC DeleteRec (RM_FileHandle *fileHandle,const RID *rid)
+RC DeleteRec(RM_FileHandle* fileHandle, const RID* rid)
 {
 	return SUCCESS;
 }
 
-RC UpdateRec (RM_FileHandle *fileHandle,const RM_Record *rec)
+RC UpdateRec(RM_FileHandle* fileHandle, const RM_Record* rec)
 {
 	return SUCCESS;
 }
 
-RC RM_CreateFile (char *fileName, int recordSize)
-{
-	return SUCCESS;
-}
-RC RM_OpenFile(char *fileName, RM_FileHandle *fileHandle)
+RC RM_CreateFile(char* fileName, int recordSize)
 {
 	return SUCCESS;
 }
 
-RC RM_CloseFile(RM_FileHandle *fileHandle)
+RC RM_OpenFile(char* fileName, RM_FileHandle* fileHandle)
+{
+	return SUCCESS;
+}
+
+RC RM_CloseFile(RM_FileHandle* fileHandle)
 {
 	return SUCCESS;
 }
