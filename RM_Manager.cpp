3f6da7b4064a60bef2054293510fa579b2831249
strangenamebc <5533827+strangenamebc@user.noreply.gitee.com>
2019-12-12 10:45:33 +0800

    完成DeleteRec（？）未测试

--- RM_Manager.cpp
+++ RM_Manager.cpp
@@ -211,6 +211,10 @@ RC InsertRec(RM_FileHandle* fileHandle, char* pData, RID* rid)
 			else
 			{
 
+				//Unpin
+
+				UnpinPage(pfPageHandle);
+
 				//释放资源并返回
 
 				free(pfPageHandle);
@@ -279,9 +283,322 @@ RC InsertRec(RM_FileHandle* fileHandle, char* pData, RID* rid)
 	}
 }
 
+//未测试
 RC DeleteRec(RM_FileHandle* fileHandle, const RID* rid)
 {
-	return SUCCESS;
+
+	//检查fileHandle的状态
+
+	if (fileHandle->bOpen == false)
+	{
+		return RM_FHCLOSED;
+	}
+
+	//检查槽位号
+
+	if (rid->slotNum >= fileHandle->recordPerPage)
+	{
+		return RM_INVALIDRID;
+	}
+
+	//打开rid所指示的页面
+
+	PF_PageHandle* targetPageHandle = (PF_PageHandle*)malloc(sizeof(PF_PageHandle));
+	targetPageHandle->bOpen = false;
+
+	RC getTargetPageRC = GetThisPage(fileHandle->pPFFileHandle, rid->pageNum, targetPageHandle);
+
+	if (getTargetPageRC == SUCCESS)
+	{
+
+		//页面获取成功
+
+		char* targetSlot = (char*)targetPageHandle->pFrame->page.pData + rid->slotNum * (fileHandle->recordSize + 8) + 2;
+		char* targetSlotData = targetSlot + 4;
+
+		short* targetSlotPrevious = (short*)targetSlot;
+		short* targetSlotNext = (short*)(targetSlot + fileHandle->recordSize + 4);
+
+		if (targetSlotPrevious[0] == 0)
+		{
+
+			//此纪录压根就是空记录
+			//Unpin
+
+			UnpinPage(targetPageHandle);
+
+			//释放资源并返回错误信息
+
+			free(targetPageHandle);
+			return RM_INVALIDRID;
+
+		}
+
+		//抹除槽位数据
+
+		for (int i = 0; i < fileHandle->recordSize; i++)
+		{
+			targetSlotData[i] = 0;
+		}
+
+		//将此槽位前后串连
+
+		if (!(fileHandle->pFirstRecord->pageNum == rid->pageNum &&
+			fileHandle->pFirstRecord->slotNum == rid->slotNum &&
+			fileHandle->pLastRecord->pageNum == rid->pageNum &&
+			fileHandle->pLastRecord->slotNum == rid->slotNum))
+		{
+
+			//此纪录不是唯一要删除的记录
+
+			if (fileHandle->pFirstRecord->pageNum == rid->pageNum &&
+				fileHandle->pFirstRecord->slotNum == rid->slotNum)
+			{
+
+				//此纪录是首条记录
+
+				fileHandle->pFirstRecord->pageNum = targetSlotNext[0];
+				fileHandle->pFirstRecord->slotNum = targetSlotNext[1];
+
+			}
+			else if (fileHandle->pLastRecord->pageNum == rid->pageNum &&
+				fileHandle->pLastRecord->slotNum == rid->slotNum)
+			{
+
+				//此纪录是末条记录
+
+				fileHandle->pLastRecord->pageNum = targetSlotPrevious[0];
+				fileHandle->pLastRecord->slotNum = targetSlotPrevious[1];
+
+			}
+			else
+			{
+
+				//此纪录是中间记录
+
+				int previousPage = targetSlotPrevious[0];
+				int previousSlot = targetSlotPrevious[1];
+				int nextPage = targetSlotNext[0];
+				int nextSlot = targetSlotNext[1];
+
+				//将前一条记录连接至后一条
+
+				if (previousPage == rid->pageNum)
+				{
+
+					//前一条记录就在此页
+
+					short* previousNext = (short*)((char*)targetPageHandle->pFrame->page.pData + (previousSlot + 1) * (fileHandle->recordSize + 8) - 2);
+					previousNext[0] = nextPage;
+					previousNext[1] = nextSlot;
+
+				}
+				else
+				{
+
+					//前一条记录不在此页
+
+					PF_PageHandle* previousPageHandle = (PF_PageHandle*)malloc(sizeof(PF_PageHandle));
+					previousPageHandle->bOpen = false;
+
+					RC getPreviousPageRC = GetThisPage(fileHandle->pPFFileHandle, previousPage, previousPageHandle);
+
+					if (getPreviousPageRC == SUCCESS)
+					{
+
+						//前一条记录所在的页打开成功
+
+						short* previousNext = (short*)((char*)previousPageHandle->pFrame->page.pData + (previousSlot + 1) * (fileHandle->recordSize + 8) - 2);
+						previousNext[0] = nextPage;
+						previousNext[1] = nextSlot;
+
+						//Unpin并标记脏页面
+
+						MarkDirty(previousPageHandle);
+						UnpinPage(previousPageHandle);
+
+						//销毁页面句柄
+
+						free(previousPageHandle);
+
+					}
+					else
+					{
+
+						//Unpin
+
+						UnpinPage(targetPageHandle);
+
+						//释放资源并返回错误信息
+
+						free(previousPageHandle);
+						free(targetPageHandle);
+						return getPreviousPageRC;
+
+					}
+				}
+
+				//将后一条记录连接至前一条
+
+				if (nextPage == rid->pageNum)
+				{
+
+					//后一条记录就在此页
+
+					short* nextPrevious = (short*)((char*)targetPageHandle->pFrame->page.pData + nextSlot * (fileHandle->recordSize + 8) + 2);
+					nextPrevious[0] = previousPage;
+					nextPrevious[1] = previousSlot;
+
+				}
+				else
+				{
+
+					//后一条记录不在此页
+
+					PF_PageHandle* nextPageHandle = (PF_PageHandle*)malloc(sizeof(PF_PageHandle));
+					nextPageHandle->bOpen = false;
+
+					RC getNextPageRC = GetThisPage(fileHandle->pPFFileHandle, nextPage, nextPageHandle);
+
+					if (getNextPageRC == SUCCESS)
+					{
+
+						//后一条记录所在的页面打开成功
+
+						short* nextPrevious = (short*)((char*)nextPageHandle->pFrame->page.pData + nextSlot * (fileHandle->recordSize + 8) + 2);
+						nextPrevious[0] = previousPage;
+						nextPrevious[1] = previousSlot;
+
+						//标记脏页面并Unpin
+
+						MarkDirty(nextPageHandle);
+						UnpinPage(nextPageHandle);
+
+						//销毁页面句柄
+
+						free(nextPageHandle);
+
+					}
+					else
+					{
+
+						//Unpin以便于淘汰
+
+						UnpinPage(targetPageHandle);
+
+						//释放资源并返回错误信息
+
+						free(nextPageHandle);
+						free(targetPageHandle);
+						return getNextPageRC;
+
+					}
+				}
+			}
+		}
+		else
+		{
+
+			//此纪录是唯一要删除的记录
+
+			fileHandle->pFirstRecord->pageNum = -1;
+			fileHandle->pFirstRecord->slotNum = -1;
+			fileHandle->pFirstRecord->bValid = false;
+
+			fileHandle->pLastRecord->pageNum = -1;
+			fileHandle->pLastRecord->slotNum = -1;
+			fileHandle->pLastRecord->bValid = false;
+
+		}
+
+		//将此槽位作为首个空槽位，以备下一次优先填充
+		//此槽位的父亲置为（0，0）
+
+		targetSlotPrevious[0] = 0;
+		targetSlotPrevious[1] = 0;
+
+		//此槽位的下一个槽位设置为原先的首个空槽位
+		//获取原先的空槽位信息
+
+		if (fileHandle->firstEmptyPage != rid->pageNum)
+		{
+
+			//原先的首个空槽位在其它页上
+
+			PF_PageHandle* oldFirstEmptyPage = (PF_PageHandle*)malloc(sizeof(PF_PageHandle));
+			oldFirstEmptyPage->bOpen = false;
+
+			RC getOldFirstEmptyPageRC = GetThisPage(fileHandle->pPFFileHandle, fileHandle->firstEmptyPage, oldFirstEmptyPage);
+
+			if (getOldFirstEmptyPageRC == SUCCESS)
+			{
+
+				//获取页面成功
+				//将新空槽位连接至旧的首个空槽位
+
+				short* oldFirstEmptySlot = (short*)(char*)oldFirstEmptyPage->pFrame->page.pData;
+				targetSlotNext[0] = fileHandle->firstEmptyPage;
+				targetSlotNext[1] = *oldFirstEmptySlot;
+
+				//Unpin以便于淘汰
+
+				UnpinPage(oldFirstEmptyPage);
+
+				//销毁页面句柄
+
+				free(oldFirstEmptyPage);
+
+			}
+			else
+			{
+
+				//Unpin
+
+				UnpinPage(targetPageHandle);
+
+				//释放资源并返回错误信息
+
+				free(targetPageHandle);
+				free(oldFirstEmptyPage);
+				return getOldFirstEmptyPageRC;
+
+			}
+		}
+		else
+		{
+
+			//原先的空槽位在此页面上
+
+			short* firstEmptySlot = (short*)(char*)targetPageHandle->pFrame->page.pData;
+			targetSlotNext[0] = rid->pageNum;
+			targetSlotNext[1] = *firstEmptySlot;
+
+		}
+
+		//将首个有空位的页面设置为此页面
+
+		fileHandle->firstEmptyPage = rid->pageNum;
+
+		//标记脏页面并Unpin
+
+		MarkDirty(targetPageHandle);
+		UnpinPage(targetPageHandle);
+
+		//销毁页面句柄，返回成功信息
+
+		free(targetPageHandle);
+		return SUCCESS;
+
+	}
+	else
+	{
+
+		//释放资源并返回错误信息
+
+		free(targetPageHandle);
+		return getTargetPageRC;
+
+	}
 }
 
 RC UpdateRec(RM_FileHandle* fileHandle, const RM_Record* rec)
