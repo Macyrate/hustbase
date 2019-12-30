1227bf77b824a114fc1aff3191c654b362fa3840
strangenamebc <5533827+strangenamebc@user.noreply.gitee.com>
2019-12-30 15:29:11 +0800

    完成一部分查询

--- QU_Manager.h
+++ QU_Manager.h
@@ -2,21 +2,30 @@
 #define __QUERY_MANAGER_H_
 #include "str.h"
 
-typedef struct SelResult{
+typedef struct SelResult {
 	int col_num;
 	int row_num;
 	AttrType type[20];	//结果集各字段的数据类型
 	int offset[20];		//结果集各字段在记录中的偏移	xmyadd20191224，为了与AutoTest保持一致
 	int length[20];		//结果集各字段值的长度
 	char fields[20][20];//最多二十个字段名，而且每个字段的长度不超过20
-	char ** res[100];	//最多一百条记录
-	SelResult * next_res;
+	char** res[100];	//最多一百条记录
+	SelResult* next_res;
 }SelResult;
 
-void Init_Result(SelResult * res);
-void Destory_Result(SelResult * res);
+typedef struct
+{
+	char attrName[21];
+	int offset;
+} Attr;
 
-RC Query(char * sql,SelResult * res);
+RC GetAttrsByRelName(char* relName, int nInputSelAttrs, RelAttr* selAttrs, int nOutputAttrs, Attr* attrs);
 
-RC Select(int nSelAttrs,RelAttr **selAttrs,int nRelations,char **relations,int nConditions,Condition *conditions,SelResult * res);
+RC Init_Result(SelResult* res, char* relName, int nSelAttrs, RelAttr* selAttrs);
+
+void Destory_Result(SelResult* res);
+
+RC Query(char* sql, SelResult* res);
+
+RC Select(int nSelAttrs, RelAttr** selAttrs, int nRelations, char** relations, int nConditions, Condition* conditions, SelResult* res);
 #endif
